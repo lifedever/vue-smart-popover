94b47d9e06b8b833925fb1852829aba77e4118db
gefangshuai <gefangshuai@163.com>
2019-06-10 17:33:09 +0800

    fix bugs

--- .deploy.sh
+++ .deploy.sh
@@ -2,6 +2,5 @@
 yarn version
 git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
-git push --all && git push --tag
-cd vue-speedy
+git push --all
 npm publish --registry http://npm.kanasinfo.cn
